75943de65f8433b142e861cdbb2b2fe6512d453d
Irina Knop <knop.irina.d@gmail.com>
2020-10-28 20:37:32 +0300

    lab 05-03 done

---
+++ lab05-03/ArrayUtils.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab05_03
+{
+    class ArrayUtils
+    {
+        public static int SumAll(int[] arr)
+        {
+            int sum = 0;
+            foreach(int a in arr)
+            {
+                sum += a;
+            }
+            return sum;
+            
+        }
+
+        public static double Average(int[] arr)
+        {
+            double result = (double)SumAll(arr) / arr.Length;
+            return result;
+        }
+
+        public static int[] NegativeAndPozitiveSums(int[] arr)
+        {
+            int pos = 0;
+            int neg = 0;
+            foreach(int a in arr)
+            {
+                if (a > 0)
+                {
+                    pos += a;
+                }
+                else if (a < 0)
+                {
+                    neg += a;
+                }
+            }
+            int[] result = { pos, neg };
+            return result;
+        }
+
+        public static int[] EvenAndOddNumSums(int[] arr)
+        {
+            int even = 0;
+            int odd = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i%2 == 0)
+                {
+                    even += arr[i];
+                }
+                else
+                {
+                    odd += arr[i];
+                }
+            }
+            int[] result = { even, odd };
+            return result;
+        }
+
+        public static int MaxIndex(int[] arr)
+        {
+            int max = int.MinValue;
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        public static int MinIndex(int[] arr)
+        {
+            int min = int.MaxValue;
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        public static int Multiply(int[] arr)
+        {
+            int minIdex = MinIndex(arr);
+            int maxIndex = MaxIndex(arr);
+            int start;
+            int stop;
+            int result = 1;
+
+            if (minIdex < maxIndex)
+            {
+                start = minIdex + 1;
+                stop = maxIndex;
+            }
+            else
+            {
+                start = maxIndex + 1;
+                stop = minIdex;
+            }
+
+            for (int i = start; i < stop; i++)
+            {
+                result *= arr[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ lab05-03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab05_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите элементы массива ");
+
+            var s = Console.ReadLine();
+            var arr = s.Split(' ');
+
+            int[] list = new int[arr.Length];
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i] = int.Parse(arr[i]);
+            }
+
+            int sum = ArrayUtils.SumAll(list);
+            Console.WriteLine("Сумма элементов массива равна {0}", sum);
+
+            double average = ArrayUtils.Average(list);
+            Console.WriteLine("Среднее значение массива {0}", average);
+
+            int[] negAndPosSums = ArrayUtils.NegativeAndPozitiveSums(list);
+            Console.WriteLine("Сумма положительных эелементов массива равна {0} и сумма отрицательных элементов массива равна {1}", negAndPosSums[0], negAndPosSums[1]);
+
+            int[] evenAndOddSums = ArrayUtils.EvenAndOddNumSums(list);
+            Console.WriteLine("Сумма четных эелементов массива равна {0} и сумма нечетных элементов массива равна {1}", evenAndOddSums[0], evenAndOddSums[1]);
+
+            int maxIndex = ArrayUtils.MaxIndex(list);
+            int minIndex = ArrayUtils.MinIndex(list);
+            Console.WriteLine("Индекс максимального элемента массива {0} и минимального элемента массива {1}", maxIndex, minIndex);
+
+            int mult = ArrayUtils.Multiply(list);
+            Console.WriteLine("Произведение элементов массива, расположенных между максимальным и минимальным элементами {0}", mult);
+
+
+
+        }
+    }
+}

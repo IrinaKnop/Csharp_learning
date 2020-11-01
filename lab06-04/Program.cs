25e688f1c4d62aa5c86e87a09e6743b6fd06b632
Irina Knop <knop.irina.d@gmail.com>
2020-11-01 19:37:34 +0300

    lab 06-04 done

---
+++ lab06-04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab06_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+
+            Console.WriteLine("Это трегольник? " + triangle.IsTriangle());
+            Console.WriteLine("Периметр треугольника " + triangle.Perimeter());
+            Console.WriteLine("Площадь треугольника " + triangle.Area());
+            Console.WriteLine(triangle.ToString());
+        }
+    }
+}

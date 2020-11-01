1f912777599fe3493b3f465cdf0d336630c51953
Irina Knop <knop.irina.d@gmail.com>
2020-11-01 19:38:50 +0300

    lab 08-04 done

---
+++ lab08-04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab08_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(3, 4, 5);
+
+            Console.WriteLine("Это трегольник? " + triangle.IsTriangle());
+            Console.WriteLine("Периметр треугольника " + triangle.Perimeter());
+            Console.WriteLine("Площадь треугольника " + triangle.Area());
+            Console.WriteLine(triangle.ToString());
+
+            Triangle equalSidesTriangle = new Triangle(5);
+
+            Console.WriteLine("Это трегольник? " + equalSidesTriangle.IsTriangle());
+            Console.WriteLine("Периметр треугольника " + equalSidesTriangle.Perimeter());
+            Console.WriteLine("Площадь треугольника " + equalSidesTriangle.Area());
+            Console.WriteLine(equalSidesTriangle.ToString());
+        }
+    }
+}
---
+++ lab08-04/Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab08_04
+{
+    class Triangle
+    {
+        private int sideA;
+        private int sideB;
+        private int sideC;
+
+        public Triangle(int a, int b, int c)
+        {
+            sideA = a;
+            sideB = b;
+            sideC = c;
+        }
+        public Triangle(int a)
+        {
+            sideA = a;
+            sideB = a;
+            sideC = a;
+        }
+
+        public int Perimeter()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        public double Area()
+        {
+            double semiper = (double)Perimeter() / 2;
+            double area = -1;
+            if (IsTriangle())
+            {
+                area = Math.Sqrt(semiper * (semiper - sideA) * (semiper - sideB) * (semiper - sideC));
+            }
+            return area;
+        }
+
+        public bool IsTriangle()
+        {
+            bool ok = false;
+            if (((sideA + sideB) > sideC) && ((sideA + sideC) > sideB) && ((sideB + sideC) > sideA))
+            {
+                ok = true;
+            }
+
+            return ok;
+        }
+
+        public override string ToString()
+        {
+            return "Сторона а = " + sideA + "; Сторона b = " + sideB + "; Сторона с = " + sideC;
+        }
+    }
+}

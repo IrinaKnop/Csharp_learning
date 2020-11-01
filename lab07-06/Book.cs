74ab51f1eed39079124f1bb88552b75b27641b9d
Irina Knop <knop.irina.d@gmail.com>
2020-11-01 19:38:21 +0300

    lab 07-06 done

---
+++ lab07-06/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab07_06
+{
+    class Book : IComparable
+    {
+        private String author;
+        private String title;
+        private int pages;
+        private int year;
+
+        public void SetBook(String author, String title, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+        }
+        public void Show()
+        {
+            Console.WriteLine("\nКнига:\n Автор: {0}\n Название: {1}\n Год издания: {2}\n {3} стр.\n", author, title, year, pages);
+        }
+
+
+
+        public int GetYear()
+        {
+            return this.year;
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            Book it = (Book)obj;
+            if (this.year == it.year) return 0;
+            else if (this.year > it.year) return 1;
+            else return -1;
+        }
+    }
+}
---
+++ lab07-06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab07_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[3];
+            books[0] = new Book();
+            books[1] = new Book();
+            books[2] = new Book();
+
+            books[0].SetBook("Г.Н. Черкесов", "Надежность технических систем с временной избыточностью", 294, 1974);
+            books[1].SetBook("Г.К. Жуков", "Воспоминвния и размышления", 736, 1969);
+            books[2].SetBook("У. Гибсон", "Нейромант", 271, 1984);
+
+            Array.Sort(books);
+
+            foreach (Book book in books)
+            {
+                book.Show();
+            }
+        }
+    }
+}

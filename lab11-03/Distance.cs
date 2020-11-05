55cbe2d560c30f2be49664caf60257a254285a2f
Irina Knop <knop.irina.d@gmail.com>
2020-11-05 15:44:39 +0300

    lab 11-03 done

---
+++ lab11-03/Distance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11_03
+{
+    class Distance
+    {
+        public int foot { get; set; }
+        public double inch { get; set; }
+
+        public Distance()
+        {
+            foot = 0;
+            inch = 0;
+        }
+
+        public Distance(int foot, double inch)
+        {
+            this.foot = foot + ((int)inch / 12);
+            this.inch = inch % 12;
+        }
+
+        public static Distance operator +(Distance d1, Distance d2) =>
+            new Distance(d1.foot + d2.foot + ((int)((d1.inch + d2.inch) / 12)), (d1.inch + d2.inch) % 12);
+        public static Distance operator -(Distance d1, Distance d2) =>
+            new Distance(((12 * d1.foot + (int)d1.inch) - (12 * d2.foot + (int)d2.inch)) / 12, ((12 * d1.foot + (int)d1.inch) - (12 * d2.foot + (int)d2.inch)) % 12);
+
+        public static bool operator > (Distance d1, Distance d2)
+        {
+            if ((12 * d1.foot + (int)d1.inch) > (12 * d2.foot + (int)d2.inch))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(Distance d1, Distance d2)
+        {
+            if ((12 * d1.foot + (int)d1.inch) < (12 * d2.foot + (int)d2.inch))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString() => (foot + "\'-" + inch + "\"");
+    }
+}
---
+++ lab11-03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Distance d1 = new Distance(6, 3);
+            Distance d2 = new Distance(1, 11);
+
+            Distance d3 = d1 + d2;
+            Distance d4 = d1 - d2;
+
+            Console.WriteLine("Величина 1 равна {0}, величина 2 равна {1}", d1, d2);
+
+            Console.WriteLine("Результат сложения " + d3);
+            Console.WriteLine("Результат вычитания " + d4);
+
+            if (d1 > d2)
+            {
+                Console.WriteLine("Величина {0} больше", d1);
+            }
+            else if (d1 < d2)
+            {
+                Console.WriteLine("Величина {0} больше", d2);
+            }
+            else
+            {
+                Console.WriteLine("Величины равны");
+            }
+        }
+    }
+}
